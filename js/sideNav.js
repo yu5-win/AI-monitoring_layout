3e892b02295f62e06aae8208766bbc77c7608c33
yu5-win <yuseng1021@gmail.com>
2021-10-25 11:02:27 +0900

    layout

---
+++ js/sideNav.js
@@ -0,0 +1,9 @@
+function openNav(){
+  document.getElementById("mySidenav").style.width = "180px";
+  document.getElementById("main").style.marginLeft = "180px";
+}
+
+function closeNav(){
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
